6b830723f06c880c416544d62ef1f89101a87356
Jan <janekg89@hotmail.de>
2017-08-04 20:19:50 +0200

    add schema settings for janeks pc, working on fill_db.py with new model.

--- flutype_webapp/settings.py
+++ flutype_webapp/settings.py
@@ -90,20 +90,19 @@ WSGI_APPLICATION = 'flutype_webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'media/db.sqlite3'),
-     },
-
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'flutype',
-    #     'USER': 'flutype_user',
-    #     'HOST': 'localhost',
-    #     'PASSWORD': 'flutype_test',
-    #     'PORT': 5432,
-    # }
+DATABASES =  {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'media/db.sqlite3'),
+#      },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flutype',
+        'USER': 'flutype_user',
+        'HOST': 'localhost',
+        'PASSWORD': 'flutype_test',
+        'PORT': 5432,
+    }
 }
 
 
