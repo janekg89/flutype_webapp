b2daf8bbfcba0e53d187340be9b11373a4c7d5c6
Matthias Koenig <konigmatt@googlemail.com>
2017-08-04 18:23:49 +0200

    latest schema generated

--- flutype_webapp/settings.py
+++ flutype_webapp/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'flutype_webapp.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'media/db.sqlite3'),
-     },
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'media/db.sqlite3'),
+   },
 
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
