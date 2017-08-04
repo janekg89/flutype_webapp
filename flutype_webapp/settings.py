36a411326a431448966bdf96fb5570cc810da821
Matthias Koenig <konigmatt@googlemail.com>
2017-08-04 17:59:34 +0200

    schema information added in docs

--- flutype_webapp/settings.py
+++ flutype_webapp/settings.py
@@ -91,16 +91,17 @@ WSGI_APPLICATION = 'flutype_webapp.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'media/db.sqlite3'),
-    },
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'media/db.sqlite3'),
+     },
 
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'postgres',
-    #     'USER': 'postgres',
-    #     'HOST': 'db',
+    #     'NAME': 'flutype',
+    #     'USER': 'flutype_user',
+    #     'HOST': 'localhost',
+    #     'PASSWORD': 'flutype_test',
     #     'PORT': 5432,
     # }
 }
