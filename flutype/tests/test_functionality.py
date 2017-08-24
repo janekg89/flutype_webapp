5cf1cc4e824525f85c15b34dbb10160349e6f6c5
Jan <janekg89@hotmail.de>
2017-08-24 13:03:07 +0200

    added forms for antibody and virus

--- flutype/tests/test_functionality.py
+++ flutype/tests/test_functionality.py
@@ -10,7 +10,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from flutype.data_management.fill_users import create_users, user_defs
 from flutype.data_management.fill_database import fill_database, path_master
-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from flutype.models import RawSpotCollection, SpotCollection, Process
 
@@ -22,7 +22,9 @@ class SeleniumTestCase(LiveServerTestCase):
         create_users(user_defs=user_defs)
 
     def setUp(self):
-        self.driver = webdriver.PhantomJS()
+        self.driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any',
+                                                   '--web-security=false'])
+        self.driver.set_window_size(1024, 768)
         create_users(user_defs=user_defs)
         fill_database(path_master=path_master, collection_ids=[
             "2017-05-19_E5_X31"
@@ -47,6 +49,7 @@ class SeleniumTestCase(LiveServerTestCase):
         self.login(expected_url)
         self.driver.find_element_by_link_text("387139").click()
         self.assertIn("https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id=387139", self.driver.current_url)
+        self.driver.save_screenshot('screenshot1.png')
 
     def test_heatmap(self):
         id = SpotCollection.objects.first().id
@@ -62,15 +65,13 @@ class SeleniumTestCase(LiveServerTestCase):
 
     def test_barplot(self):
         id = SpotCollection.objects.first().id
-        expected_url = '%s%s%s' %(self.live_server_url,'/flutype/qspotcollection/', id)
+        expected_url = '%s%s%s%s' %(self.live_server_url,'/flutype/qspotcollection/', id,'/')
         self.login(expected_url)
         # keep a watch on jQuery 'active' attribute
         # page should be stable enough now, and we can perform desired actions
-        self.driver.implicitly_wait(10)
         elem = WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located((By.ID, 'myDiv')))
         #java_script = '$.ajax({ method: "GET", url: endpoint, success: function(data){ dataPlot = data; Plotly.newPlot('myDiv', Chart(dataPlot)); }, error: function(error_data){ console.log("error") console.log(error_data)  }});'
         #dataPlot = self.driver.execute_async_script("dataPlot",elem)
-        sleep(2)
         self.driver.save_screenshot('screenshot.png')
 
         retval = self.driver.execute_script("return Chart(dataPlot);",elem)
