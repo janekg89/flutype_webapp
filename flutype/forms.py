6865a8586b6efdec6fb141d298fbd96bdc60b6f1
Jan <janekg89@hotmail.de>
2017-08-31 18:38:41 +0200

    working on forms

--- flutype/forms.py
+++ flutype/forms.py
@@ -1,8 +1,17 @@
+
 from django import forms
 from .models import Peptide, PeptideBatch, Virus, VirusBatch, Antibody, AntibodyBatch, \
     ProcessStep, Step, Spotting, Washing, Drying, Quenching, Blocking, Scanning, Incubating, \
     Process
 
+class OrderedModelMultipleChoiceField(forms.ModelMultipleChoiceField):
+    def clean(self, value):
+        qs = super(OrderedModelMultipleChoiceField, self).clean(value)
+        clauses = ' '.join(['WHEN id=%s THEN %s' % (pk, i) for i, pk in enumerate(value)])
+        return qs.filter(pk__in=value).extra(
+            select={'ordering': 'CASE %s END' % clauses},
+            order_by=('ordering',)
+        )
 
 class PeptideForm(forms.ModelForm):
     class Meta:
@@ -13,31 +22,29 @@ class PeptideForm(forms.ModelForm):
 class VirusForm(forms.ModelForm):
     class Meta:
         model = Virus
-        fields = "__all__"
+        fields = ['sid','tax_id', 'subtype', "isolation_country", "collection_date", "strain", "link_db", "comment"]
 
 
 class AntibodyForm(forms.ModelForm):
     class Meta:
         model = Antibody
-        fields = "__all__"
+        fields = ['sid', 'target','name', 'link_db', 'comment']
 
-
+batch_fields = ['sid', 'ligand','concentration', 'buffer', 'ph', 'purity', 'produced_by','production_date', 'comment']
 class PeptideBatchForm(forms.ModelForm):
     class Meta:
         model = PeptideBatch
-        fields = '__all__'
-
-
+        fields = batch_fields
 class VirusBatchForm(forms.ModelForm):
     class Meta:
         model = VirusBatch
-        fields = '__all__'
+        fields = batch_fields
 
 
 class AntibodyBatchForm(forms.ModelForm):
     class Meta:
         model = AntibodyBatch
-        fields = '__all__'
+        fields = batch_fields
 
 
 class ProcessStepForm(forms.ModelForm):
@@ -45,47 +52,55 @@ class ProcessStepForm(forms.ModelForm):
         model = ProcessStep
         fields = '__all__'
 
-
 class StepForm(forms.ModelForm):
     class Meta:
         model = Step
         fields = '__all__'
 
 
+
 class SpottingForm(forms.ModelForm):
     class Meta:
         model = Spotting
-        fields = '__all__'
+        fields = ['sid', 'method', 'temperature', 'comment']
 
 
 class WashingForm(forms.ModelForm):
     class Meta:
         model = Washing
-        fields = '__all__'
+        fields = ['sid', 'method', 'substance', 'temperature', 'comment']
 
 
 class DryingForm(forms.ModelForm):
     class Meta:
         model = Drying
-        fields = '__all__'
+        fields = ['sid', 'method', 'substance', 'temperature', 'comment']
 
 
 class QuenchingForm(forms.ModelForm):
     class Meta:
         model = Quenching
-        fields = '__all__'
+        fields = ['sid', 'method', 'substance', 'temperature', 'comment']
 
 
 class BlockingForm(forms.ModelForm):
     class Meta:
         model = Blocking
-        fields = '__all__'
+        fields = ['sid', 'method', 'substance', 'temperature', 'comment']
 
 
 class IncubatingForm(forms.ModelForm):
     class Meta:
         model = Incubating
-        fields = '__all__'
+        fields = ['sid', 'method', 'temperature', 'comment']
+        labels ={
+            'temperature':'Temperature',
+        }
+        help_texts = {
+            'temperature': 'In Celsius.',
+        }
+
+
 
 
 class ScanningForm(forms.ModelForm):
@@ -95,6 +110,10 @@ class ScanningForm(forms.ModelForm):
 
 
 class ProcessForm(forms.ModelForm):
+    orderer_steps = OrderedModelMultipleChoiceField(Step.objects.all())
     class Meta:
         model = Process
         fields = '__all__'
+
+
+
