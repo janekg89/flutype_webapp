0a54715e387ec340491252a343772d530ab17c4c
Jan <janekg89@hotmail.de>
2017-09-05 19:59:09 +0200

    working on test for inout output

--- flutype/tests/test_import_export.py
+++ flutype/tests/test_import_export.py
@@ -1,4 +1,4 @@
-from flutype.data_management.fill_database import Database,fill_database, path_master, path_master2
+from flutype.data_management.fill_database import Database,fill_database, path_master
 from flutype.data_management.fill_master import Master
 from flutype.data_management.fill_users import create_users, user_defs
 
@@ -13,14 +13,11 @@ class LoadDataFromDataBase(TestCase):
             "2017-05-19_E5_X31"
         ])
 
-
-
     def tearDown(self):
         create_users(user_defs=None, delete_all=True)
 
     def test_load_database_from_master(self):
-        print(Database.load_database())
-        return Database.load_database()
+        self.assertEqual(Database.load_database(), 0)
 
 
 
