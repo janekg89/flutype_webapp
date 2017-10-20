b9ef1b7f1e8fd6cb2a7c9e2b5eee9994ddaa4718
Matthias Koenig <konigmatt@googlemail.com>
2017-10-20 16:03:40 +0200

    adding first glossary terms

---
+++ create_db.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+########################################################
+# Setup the database
+#
+# Deletes old database and recreates all data.
+########################################################
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# delete old database files & uploads
+cd $DIR
+rm ./media/db.sqlite3
+rm -rf flutype/migrations/*
+
+# clean setup
+python manage.py makemigrations
+python manage.py makemigrations flutype
+python manage.py migrate
+
+# clean everything
+echo "* Remove cache *"
+rm -rf media/CACHE/*
+
+echo "* Upload archives *"
+# python manage.py flush
+python flutype/data_management/fill_users.py
+python flutype/data_management/fill_database.py
