28fbdba4c0267834da0bd3d43144d78104bed8b9
Matthias Koenig <konigmatt@googlemail.com>
2017-11-17 02:23:12 +0100

    fixing tests due to user script

--- flutype/data_management/fill_users.py
+++ flutype/data_management/fill_users.py
@@ -44,6 +44,9 @@ def create_users(user_defs, delete_all=True):
     :param delete_all: deletes all existing users
     :return:
     """
+    if not user_defs:
+        user_defs = []
+
     # deletes all users
     if delete_all:
         User.objects.all().delete()
