578f8ddfc786da73bccb55aed6d4c8099a4d1390
Jan <janekg89@hotmail.de>
2017-11-17 16:09:57 +0100

    admin added to navbar

--- flutype_webapp/urls.py
+++ flutype_webapp/urls.py
@@ -13,10 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
 from django.conf.urls.static import static
-from django.conf import settings
 
 from django.conf import settings
 from django.conf.urls import include, url
@@ -27,7 +24,6 @@ from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^', include('flutype.urls')),
     url(r'^$', RedirectView.as_view(pattern_name='index', permanent=False)),
     url(r'^login/$', auth_views.login, name='login'),
